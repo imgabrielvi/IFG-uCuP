2673d0430a4ba6c8907f61bafeb4db9c6cfb39c2
Victor Gabriel Pantaleão Santos <53866405+imgabrielvi@users.noreply.github.com>
2019-09-17 11:45:57 -0300

    Update Controle.ino

--- Controle.ino
+++ Controle.ino
@@ -1,7 +1,12 @@
 int B[11];
 
 void setup(){
-
+   pinMode(B[2], INPUT_PULLUP); pinMode(B[3], INPUT_PULLUP);
+   pinMode(B[6], INPUT_PULLUP); pinMode(B[8], INPUT_PULLUP);
+   pinMode(B[9], INPUT_PULLUP); pinMode(B[0], OUTPUT);
+   pinMode(B[1], OUTPUT); pinMode(B[4], OUTPUT);
+   pinMode(B[5], OUTPUT); pinMode(B[7], OUTPUT);
+   pinMode(B[10], OUTPUT);
 }
 
 void loop(){
