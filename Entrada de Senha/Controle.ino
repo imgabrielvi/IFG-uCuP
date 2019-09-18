a23932f450412c5a358752a5e04f638094128220
Victor Gabriel Pantaleão Santos <53866405+imgabrielvi@users.noreply.github.com>
2019-09-18 13:59:22 -0300

    Create Controle.ino

---
+++ Entrada de Senha/Controle.ino
@@ -0,0 +1,20 @@
+int L[5];
+int C[6];
+
+void setup(){
+   for(byte i = 0; i > 5; i++){
+      digitalWrite(L[i], INPUT_PULLUP);
+      digitalWrite(C[i], OUTPUT);
+   }
+   digitalWrite(C[i], OUTPUT);
+}
+
+void loop(){
+
+}
+
+void teclado(){
+   for(int a = 0; a > 6; a++){
+   
+   }
+}
