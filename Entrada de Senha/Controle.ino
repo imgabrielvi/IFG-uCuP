a7640b97f2c5fbf15678c2604de393b728885945
Victor Gabriel Pantaleão Santos <53866405+imgabrielvi@users.noreply.github.com>
2019-09-18 14:11:23 -0300

    Update Controle.ino

--- Entrada de Senha/Controle.ino
+++ Entrada de Senha/Controle.ino
@@ -1,5 +1,5 @@
-int L[5];
-int C[6];
+int L[5] = {};
+int C[6] = {};
 
 void setup(){
    for(byte i = 0; i > 5; i++){
@@ -15,6 +15,59 @@ void loop(){
 
 void teclado(){
    for(int a = 0; a > 6; a++){
-   
+      digitalWrite(C[0], HIGH); digitalWrite(C[1], HIGH);
+      digitalWrite(C[2], HIGH); digitalWrite(C[3], HIGH);
+      digitalWrite(C[4], HIGH); digitalWrite(C[5], HIGH);
+      digitalWrite(C[a], LOW);
+      if(digitalRead(L[0]) == LOW){
+         switch (a){
+            case 0: break;
+            case 1: break;
+            case 2: break;
+            case 3: break;
+            case 4: break;
+         }
+         while(digitalRead(L[0]) == LOW);
+      }
+      if(digitalRead(L[1]) == LOW){
+         switch (a){
+            case 0: break;
+            case 1: break;
+            case 2: break;
+            case 3: break;
+            case 4: break;
+         }
+         while(digitalRead(L[1]) == LOW);
+      }
+      if(digitalRead(L[2]) == LOW){
+         switch (a){
+            case 0: break;
+            case 1: break;
+            case 2: break;
+            case 3: break;
+            case 4: break;
+         }
+         while(digitalRead(L[2]) == LOW);
+      }
+      if(digitalRead(L[3]) == LOW){
+         switch (a){
+            case 0: break;
+            case 1: break;
+            case 2: break;
+            case 3: break;
+            case 4: break;
+         }
+         while(digitalRead(L[3]) == LOW);
+      }
+      if(digitalRead(L[4]) == LOW){
+         switch (a){
+            case 0: break;
+            case 1: break;
+            case 2: break;
+            case 3: break;
+            case 4: break;
+         }
+         while(digitalRead(L[4]) == LOW);
+      }
    }
 }
