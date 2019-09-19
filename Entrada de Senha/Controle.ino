628cde36e40a24366d28be584403581c28c4174d
Victor Gabriel Pantaleão Santos <53866405+imgabrielvi@users.noreply.github.com>
2019-09-19 08:44:05 -0300

    Update Controle.ino

--- Entrada de Senha/Controle.ino
+++ Entrada de Senha/Controle.ino
@@ -8,17 +8,18 @@ int L[5] = {},
     C[6] = {},
     cont = 0,
     digito;
+byte secao = 0;
 
 void setup(){
-   for(byte i = 0; i < 5; i++){
-      digitalWrite(L[i], INPUT_PULLUP);
-      digitalWrite(C[i], OUTPUT);
+   for(byte a = 0; a < 5; a++){
+      digitalWrite(L[a], INPUT_PULLUP);
+      digitalWrite(C[a], OUTPUT);
    }
-   digitalWrite(C[i], OUTPUT);
-   for(byte a = 0; a < 10; a++){
-      for(byte b = 0; b < 2; b++){
-         usuario[a].ID = EEPROM.read((4*a)+b);
-         usuario[a].senha = EEPROM.read((4*a)+b+2);
+   digitalWrite(C[5], OUTPUT);
+   for(byte b = 0; b < 10; b++){
+      for(byte c = 0; c < 2; c++){
+         usuario[b].ID = EEPROM.read((4*b)+c);
+         usuario[b].senha = EEPROM.read((4*b)+c+2);
       }
    }
 }
@@ -28,13 +29,13 @@ void loop(){
 }
 
 void teclado(){
-   for(byte a = 0; a < 6; a++){
+   for(byte d = 0; d < 6; d++){
       digitalWrite(C[0], HIGH); digitalWrite(C[1], HIGH);
       digitalWrite(C[2], HIGH); digitalWrite(C[3], HIGH);
       digitalWrite(C[4], HIGH); digitalWrite(C[5], HIGH);
-      digitalWrite(C[a], LOW);
+      digitalWrite(C[d], LOW);
       if(digitalRead(L[0]) == LOW){
-         switch (a){
+         switch (d){
             case 0: numero(1); break;
             case 1: numero(2); break;
             case 2: numero(3); break;
@@ -42,7 +43,7 @@ void teclado(){
          while(digitalRead(L[0]) == LOW);
       }
       if(digitalRead(L[1]) == LOW){
-         switch (a){
+         switch (d){
             case 0: numero(4); break;
             case 1: numero(5); break;
             case 2: numero(6); break;
@@ -50,7 +51,7 @@ void teclado(){
          while(digitalRead(L[1]) == LOW);
       }
       if(digitalRead(L[2]) == LOW){
-         switch (a){
+         switch (d){
             case 0: numero(7); break;
             case 2: numero(9);break;
             case 3: numero(8);break;
@@ -58,13 +59,13 @@ void teclado(){
          while(digitalRead(L[2]) == LOW);
       }
       if(digitalRead(L[3]) == LOW){
-         switch (a){
+         switch (d){
             case 3: numero(0); break;
          }
          while(digitalRead(L[3]) == LOW);
       }
       if(digitalRead(L[4]) == LOW){
-         switch (a){
+         switch (d){
             case 4: comando('#'); break;
             case 5: comando('*'); break;
          }
@@ -82,4 +83,19 @@ void numero(int valor){
 void comando(char valor){
    if(valor == '#') digito = digito/10;
    if(valor == '*');
+}
+
+bool validacao(){
+   if(secao){
+      if(usuario[secao - 1].senha == digito);
+   }
+   else{
+      for(byte e = 0; e < 10; e++){
+         if(usuario[e].ID == digito) break;
+      }
+      if(e == 10) return false;
+      else{
+         secao = e+1;
+         return true;
+      }
 }
